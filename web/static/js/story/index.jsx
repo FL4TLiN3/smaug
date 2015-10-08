f7a8004111de793c7329f2978c61d2344373f21f
HiranoMasaaki <hirano.masaaki@dena.jp>
2015-10-08 17:32:51 +0900

    Style story page

--- web/static/js/story/index.jsx
+++ web/static/js/story/index.jsx
@@ -17,13 +17,17 @@ const Story = React.createClass({
 
   render: function () {
     return (
-      <div className="col-md-10 ">
-        <div className="story" onClick={ this.showStory }>
-          <div className="tag">{ this.state.tag }</div>
-          <div className="info">
-            <p className="author">{ this.state.author }</p>
-            <p className="title">{ this.state.title }</p>
+      <div className="col-md-10 story">
+        <div className="row">
+          <div className="col-xs-12 cover">
+            <img src={ '/images/' + this.state.cover } />
           </div>
+          <header className="col-md-8 col-md-offset-2">
+            <p className="tag">{ this.state.tag }</p>
+            <p className="author">{ this.state.author }</p>
+            <h1 className="title">{ this.state.title }</h1>
+          </header>
+          <p className="col-md-8 col-md-offset-2 content">{ this.state.description }</p>
         </div>
       </div>
     );
