d05f878a070fcb9d8b088e211cabbba6615b876a
HiranoMasaaki <hirano.masaaki@dena.jp>
2015-10-08 16:59:45 +0900

    Implement Router

--- web/static/js/app.jsx
+++ web/static/js/app.jsx
@@ -1,21 +1,18 @@
-import Main from './main';
+
 import Navbar from './navbar';
 import Sidebar from './sidebar';
-
-const App = React.createClass({
-  getInitialState: function () {
-    return {};
-  },
-
-  render: function () {
-    return (
-      <div className="row">
-        <Sidebar />
-        <Main />
-      </div>
-    );
-  }
-});
-
-React.render(<App />, document.getElementById('content'));
 React.render(<Navbar />, document.getElementById('navbar'));
+React.render(<Sidebar />, document.getElementById('sidebar'));
+
+
+import Router from './router';
+import Main from './main';
+import Story from './story';
+
+const router = new Router(document.getElementById('content'));
+router
+.route('/', Main)
+.route('/stories/:id', Story)
+.run();
+
+
