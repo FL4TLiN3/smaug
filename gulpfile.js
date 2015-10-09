19ef9cf0ca37cbc3606275b9aaf1283ac4a4f94e
HiranoMasaaki <hirano.masaaki@dena.jp>
2015-10-09 11:46:47 +0900

    Fix build tasks; build-js, build-vendor-js

--- gulpfile.js
+++ gulpfile.js
@@ -10,42 +10,37 @@ var babelify = require('babelify')
   , rename = require("gulp-rename")
   , sass = require('gulp-sass')
   , sourcemaps = require('gulp-sourcemaps')
-  , uglify = require('gulp-uglify')
+  , uglify = require('uglifyify')
   , watch = require('gulp-watch')
   , buffer = require('vinyl-buffer')
   , source = require("vinyl-source-stream");
 
-gulp.task('build-js', function() {
-  browserify({ entries: 'web/static/js/app.jsx', extensions: ['.js', '.jsx'] })
-    .transform(babelify)
-    .bundle()
-    .on('error', function (error) {
-      console.error(error.stack);
-    })
-    .pipe(source('app.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init())
-      .pipe(uglify())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('priv/static/js'));
+gulp.task('build-js:development', function() {
+  return browserify({
+    entries: 'web/static/js/app.jsx',
+    extensions: ['.js', '.jsx'],
+    debug: true
+  })
+  .transform(babelify)
+  .transform(uglify)
+  .bundle().on('error', function (error) { console.error(error.stack); })
+  .pipe(source('app.js'))
+  .pipe(buffer())
+  .pipe(gulp.dest('priv/static/js'));
 });
 
 gulp.task('build-vendor-js', function() {
-  gulp.src('web/static/vendor/**/*.js')
-    .pipe(sourcemaps.init())
-      .pipe(order([
-        "jquery.js",
-        "bootstrap.js",
-        "react.js"
-      ]))
-      .pipe(concat('vendor.js'))
-      .pipe(uglify())
-    .pipe(sourcemaps.write('./'))
+  return gulp.src(['web/static/vendor/jquery.js', 'web/static/vendor/bootstrap.js'])
+    .pipe(order([
+      "jquery.js",
+      "bootstrap.js"
+    ]))
+    .pipe(concat('vendor.js'))
     .pipe(gulp.dest('priv/static/js'));
 });
 
 gulp.task('build-style', function () {
-  gulp.src('web/static/css/app.scss')
+  return gulp.src('web/static/css/app.scss')
     .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       .pipe(autoprefixer())
@@ -57,17 +52,14 @@ gulp.task('build-style', function () {
 
 gulp.task('watch-js', function() {
   watch([
-    'node_modules/**/*.*',
-    'bower_components/**/*.*',
     'web/static/js/**/*.*'
   ], function () {
-    gulp.start('build-js');
+    gulp.start('build-js:development');
   });
 });
 
 gulp.task('watch-style', function () {
   watch([
-    'bower_components/**/*.*',
     'web/static/css/**/*.*'
   ], function () {
     gulp.start('build-style');
@@ -79,7 +71,7 @@ gulp.task('sync-assets', function() {
 });
 
 gulp.task('default', [
-  'build-js',
+  'build-js:development',
   'build-vendor-js',
   'build-style',
   'watch-js',
