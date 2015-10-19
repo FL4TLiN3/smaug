ac9ee5529b4e46a171c83d14bef5e8ab243fcf04
HiranoMasaaki <hirano.masaaki@dena.jp>
2015-10-19 15:00:55 +0900

    Implement github login

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -63,53 +63,64 @@ defmodule Smaug.AuthController do
   def auth_callback_github(conn, %{"code" => code}) do
     token = GitHub.get_token!(code: code)
     github_user = OAuth2.AccessToken.get!(token, "/user")
-    params = %{
+    user = Repo.one(from u in User, where: u.email == ^github_user["email"])
+
+    if user do
+      conn
+      |> login_with_github(user, token, github_user)
+    else
+      conn
+      |> signup_with_github(token, github_user)
+    end
+
+  end
+
+  defp login_with_github(conn, user, token, github_user) do
+    conn
+    |> put_session(:user, user)
+    |> redirect(to: page_path(conn, :spa))
+  end
+
+  defp signup_with_github(conn, token, github_user) do
+    user = Repo.insert! User.changeset(%User{}, %{
       "email": github_user["email"],
       "github_access_token": token.access_token
+    })
+
+    Repo.insert! %UserProfile{
+      "user_id": user.id,
+      "name": github_user["name"],
+      "location": github_user["location"],
+      "bio": github_user["bio"]
     }
 
-    changeset = User.changeset(%User{}, params)
-    case Repo.insert(changeset) do
-      {:ok, user} ->
-        Repo.insert! %UserProfile{
-          "user_id": user.id,
-          "name": github_user["name"],
-          "location": github_user["location"],
-          "bio": github_user["bio"]
-        }
-
-        conn
-        |> put_flash(:info, "User created successfully.")
-        |> put_session(:user, user)
-        |> redirect(to: auth_path(conn, :profile))
-      {:error, changeset} ->
-        conn
-        |> render(:new, changeset: changeset)
-    end
+    conn
+    |> put_flash(:info, "User created successfully.")
+    |> put_session(:user, user)
+    |> redirect(to: auth_path(conn, :profile))
   end
 
   def profile(conn, _params) do
-    case user = conn |> get_session :user do
-      %User{id: id} ->
-        user_profile = Repo.one(from up in UserProfile, where: up.user_id == ^id)
-        changeset = UserProfile.changeset(user_profile)
-        IO.inspect changeset
+    user = get_session(conn, :user)
+    user_profile = Repo.one(from up in UserProfile, where: up.user_id == ^user.id)
+    changeset = UserProfile.changeset(user_profile)
 
-        conn
-        |> render(:profile, changeset: changeset)
-    end
+    conn
+    |> render(:profile, user_profile: user_profile, changeset: changeset)
   end
 
   def update_profile(conn, %{"user_profile" => user_profile_params}) do
-    changeset = UserProfile.changeset(%UserProfile{}, user_profile_params)
-    case Repo.insert(changeset) do
+    user_profile = Repo.get!(UserProfile, user_profile_params["user_id"])
+    changeset = UserProfile.changeset(user_profile, user_profile_params)
+
+    case Repo.update(changeset) do
       {:ok, _user_profile} ->
         conn
         |> put_flash(:info, "UserProfile created successfully.")
         |> redirect(to: page_path(conn, :spa))
       {:error, changeset} ->
         conn
-        |> render(:profile, changeset: changeset)
+        |> render(:profile, user_profile: user_profile, changeset: changeset)
     end
   end
 
