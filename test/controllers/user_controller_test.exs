46afe6b5eedff5fb944b2c470838317a59f13c0b
HiranoMasaaki <hirano.masaaki@dena.jp>
2015-10-21 16:19:19 +0900

    Fix test; user

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -2,7 +2,17 @@ defmodule Smaug.UserControllerTest do
   use Smaug.ConnCase
 
   alias Smaug.User
-  @valid_attrs %{access_secret: "some content", access_secret_generated_at: "2010-04-17 14:00:00", access_token: "some content", access_token_expires_at: "2010-04-17 14:00:00", email: "some content", facebook_access_token: "some content", name: "some content", password: "some content"}
+  @valid_attrs %{
+    email: "john@aktsk.jp",
+    name: "John Smith",
+    password: "foobar",
+    facebook_access_token: "f165521737c7286a7a817b8015f25152c623137e",
+    github_access_token: "f165521737c7286a7a817b8015f25152c623137e",
+    access_secret: "12CD955B78D28CB1CB0B0088B9A0960B86559B227778BF2127DEABB176E23F63",
+    access_secret_generated_at: "2015-10-20 09:30:13",
+    access_token: "B6E1AEC85126787DC07141571083F82DBB0D6D025F3B7EABAAE9FC1F6EB4F3DD",
+    access_token_expires_at: "2015-11-19 09:30:13"
+  }
   @invalid_attrs %{}
 
   setup do
@@ -23,7 +33,7 @@ defmodule Smaug.UserControllerTest do
   test "creates resource and redirects when data is valid", %{conn: conn} do
     conn = post conn, user_path(conn, :create), user: @valid_attrs
     assert redirected_to(conn) == user_path(conn, :index)
-    assert Repo.get_by(User, @valid_attrs)
+    assert Repo.get_by(User, %{email: "john@aktsk.jp"})
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
@@ -53,7 +63,7 @@ defmodule Smaug.UserControllerTest do
     user = Repo.insert! %User{}
     conn = put conn, user_path(conn, :update, user), user: @valid_attrs
     assert redirected_to(conn) == user_path(conn, :show, user)
-    assert Repo.get_by(User, @valid_attrs)
+    assert Repo.get_by(User, %{email: "john@aktsk.jp"})
   end
 
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
